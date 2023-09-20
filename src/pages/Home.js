78696c035806711f24b9ee48c461c5af98ad21d5
Alpha DIOP <alphakhoss@gmail.com>
2023-09-20 16:21:00 +0000

    init: home-page

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import Feature from "../components/Feature";
+import Hero from "../components/Hero";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Feature />
+    </>
+  );
+}
+
+export default Home;
